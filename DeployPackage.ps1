c9ed28ddf9bf7f6ba343953536467f947236f26e
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-05-07 17:03:15 -0500

    Removed CmdletBinding.

--- DeployPackage.ps1
+++ DeployPackage.ps1
@@ -1,5 +1,4 @@
-[CmdletBinding()]
-param ([Parameter(Mandatory)][String]$username, [Parameter()][String]$password)
+param ([Parameter(Mandatory)][SecureString]$username, [Parameter()][SecureString]$password)
 
 if ($password) { Write-Host "Password stored." }
 if (!$password) { Write-Host "Password not stored." } 
@@ -10,8 +9,8 @@ Write-Host "Password - " $password
 Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
 Install-Module SharePointPnPPowerShellOnline -Scope "CurrentUser" -Verbose -Force
 
-$sp = $password | ConvertTo-SecureString -AsPlainText -Force
-# $sp = $password
+# $sp = $password | ConvertTo-SecureString -AsPlainText -Force
+$sp = $password
 $plainCred = New-Object system.management.automation.pscredential -ArgumentList $username, $sp
 
 Connect-PnPOnline -Url $env:siteUrl -Credentials $plainCred
