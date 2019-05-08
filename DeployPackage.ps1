631fae41779943ed03e5feeb023646b61458dc9b
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-05-08 15:46:37 -0500

    Added new test / production switch.

--- DeployPackage.ps1
+++ DeployPackage.ps1
@@ -3,10 +3,19 @@ param ([Parameter()]$password)
 Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
 Install-Module SharePointPnPPowerShellOnline -Scope "CurrentUser" -Verbose -Force
 
+if ($env:environment -eq "production") {
+    $siteUrl = $env:productionSiteUrl
+}
+else {
+    $siteUrl = $env:testSiteUrl
+}
+
+Write-Host "SiteUrl - " $siteUrl
+
 $sp = $password | ConvertTo-SecureString -AsPlainText -Force
 $plainCred = New-Object system.management.automation.pscredential -ArgumentList $env:username, $sp
 
-Connect-PnPOnline -Url $env:siteUrl -Credentials $plainCred
+Connect-PnPOnline -Url $siteUrl -Credentials $plainCred
 
 $packagePath =  "./" + $env:dropPath + "/drop/sharepoint/solution/" + $env:packageName
 Add-PnPApp $packagePath -Scope Site -Overwrite -Publish
