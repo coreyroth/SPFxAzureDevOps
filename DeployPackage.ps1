125c4d15907500ae1852e019b517f870b9957bac
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-05-08 11:34:05 -0500

    Updated script to user secure string.

--- DeployPackage.ps1
+++ DeployPackage.ps1
@@ -1,4 +1,4 @@
-param ([Parameter(Mandatory)]$username, [Parameter()]$password)
+param ([Parameter()]$username, [Parameter()]$password)
 
 if ($password) { Write-Host "Password stored." }
 if (!$password) { Write-Host "Password not stored." } 
@@ -9,8 +9,8 @@ Write-Host "Password - " $password
 Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
 Install-Module SharePointPnPPowerShellOnline -Scope "CurrentUser" -Verbose -Force
 
-# $sp = $password | ConvertTo-SecureString -AsPlainText -Force
-$sp = $password
+$sp = $password | ConvertTo-SecureString -AsPlainText -Force
+# $sp = $password
 $plainCred = New-Object system.management.automation.pscredential -ArgumentList $username, $sp
 
 Connect-PnPOnline -Url $env:siteUrl -Credentials $plainCred
