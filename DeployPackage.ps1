a8369bfdc49141082abc6f6ebce3fef53c4c48cd
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-05-08 11:53:20 -0500

    Updated script.

--- DeployPackage.ps1
+++ DeployPackage.ps1
@@ -3,8 +3,8 @@ param ([Parameter()]$username, [Parameter()]$password)
 if ($password) { Write-Host "Password stored." }
 if (!$password) { Write-Host "Password not stored." } 
 
-Write-Host "Username 2 - " $username
-Write-Host "Password 2 - " $password
+Write-Host "Username 3 - " $username
+Write-Host "Password 3 - " $password
 
 Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
 Install-Module SharePointPnPPowerShellOnline -Scope "CurrentUser" -Verbose -Force
@@ -15,5 +15,5 @@ $plainCred = New-Object system.management.automation.pscredential -ArgumentList 
 
 Connect-PnPOnline -Url $env:siteUrl -Credentials $plainCred
 
-$packagePath =  "./" + $env:dropPath + "/drop/sharepoint/solution" + $env:packageName
+$packagePath =  "./" + $env:dropPath + "/drop/sharepoint/solution/" + $env:packageName
 Add-PnPApp $packagePath -Scope Site -Overwrite -Publish
