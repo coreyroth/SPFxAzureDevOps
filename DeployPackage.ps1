9375f0bac21fad08636fbdeda93793fa9630024a
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-05-08 11:46:57 -0500

    Updated script.

--- DeployPackage.ps1
+++ DeployPackage.ps1
@@ -3,8 +3,8 @@ param ([Parameter()]$username, [Parameter()]$password)
 if ($password) { Write-Host "Password stored." }
 if (!$password) { Write-Host "Password not stored." } 
 
-Write-Host "Username - " $username
-Write-Host "Password - " $password
+Write-Host "Username 2 - " $username
+Write-Host "Password 2 - " $password
 
 Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
 Install-Module SharePointPnPPowerShellOnline -Scope "CurrentUser" -Verbose -Force
